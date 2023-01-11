c192d40062ba33c4ff5158056e9af1152690beb8
Mateo <mtaycruz@gmail.com>
2023-01-11 18:33:06 -0500

    header added but no links to login or shopping cart yet

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App';
 import './index.css';
-import Catalog from './components/Catalog.js';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Catalog />
+    <App />
   </React.StrictMode>
 );
 
