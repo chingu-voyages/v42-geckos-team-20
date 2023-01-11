c0a309b31e827a2db1fad439085c2a8f7b0ab67f
robert <matthewrueter51@gmail.com>
2023-01-11 14:37:00 -0600

    add key to mapped products list.

--- src/components/Catalog.js
+++ src/components/Catalog.js
@@ -7,7 +7,8 @@ console.log(products);
 export  default function Catalog (){
 	
 	const allProducts = products.map((product)=>
-		<ProductCard  
+		<ProductCard 
+		key={product.id} 
 		name={product.name} 
 		seller={product.seller.name} 
 		image={product.images[0]}/>
