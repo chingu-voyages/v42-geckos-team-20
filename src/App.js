36db130963d98a1bbca302ea6ebb5f0ac5b383eb
NicholasK <nicholask1@outlook.com>
2023-01-13 14:24:07 -0500

    Added CategoryFilters component to App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 
 import Catalog from './components/Catalog.js';
 import Heading  from './components/Heading.js';
+import CategoryFilters from './components/CategoryFilters';
 
 function App() {
   return (
 
     <div className="App">
       <Heading />
+      <CategoryFilters />
       <Catalog />
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import App from './App';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
