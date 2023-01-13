e71d7d4aa4cc4db8dc6e43e8dc319d0872871f9e
Julie Evans <juliemevans17@gmail.com>
2023-01-13 13:33:01 -0600

    add catalog array to App component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,13 @@ import './App.css';
 import Catalog from './components/Catalog.js';
 import Heading  from './components/Heading.js';
 
+import products from './data/products'
+
+// get an array of categories from products json
+const categories = products.map(product => product.categories).flat()
+const uniqueCategories = [...new Set(categories)]
+console.log(uniqueCategories)
+
 function App() {
   return (
 
