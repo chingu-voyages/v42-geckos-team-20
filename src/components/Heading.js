8893e933981b82c84edc4fa444b66c70fb6a459d
Julie Evans <juliemevans17@gmail.com>
2023-01-15 21:05:34 -0600

    add home component for landing page content and add subheader component with updates to styles

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -1,29 +1,35 @@
-import React from 'react'
+import React from 'react';
 
-import { Box } from '@mui/system';
+import { Box } from '@mui/material';
 
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
 const Heading = () => {
   return (
-    <Box sx={{ 
-      
-      display: 'flex',
-      justifyContent: 'space-between',
-      alignItems: 'center',
-      borderBottom: '1px solid black',
-      padding: '0 5px 0 5px'
-      
-      }}>
-      <h1>Nearby Markets</h1>
-      <Box sx={{
-
-        width: 70,
+    <Box 
+      sx={{ 
         display: 'flex',
-        justifyContent: 'space-between'
-
-      }}>
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        borderBottom: '1px solid black',
+        padding: '0 10px',
+        position: 'fixed',
+        top: 0,
+        width: 'calc(100% - 20px)',
+        height: '80px',
+        zIndex: 2,
+        bgcolor: 'background.paper'
+      }}
+    >
+      <h1>Nearby Markets</h1>
+      <Box 
+        sx={{
+          width: 70,
+          display: 'flex',
+          justifyContent: 'space-between'
+        }}
+      >
         <AccountCircleIcon />
         <ShoppingCartIcon />
       </Box>
---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import Catalog from './Catalog.js';
+import Heading  from './Heading.js';
+import SubHeader from './SubHeader.js';
+
+const Home = () => {
+  return (
+    <>
+      <Heading />
+      <SubHeader />
+      
+      <Catalog />
+    </>
+  )
+}
+
+export default Home;
