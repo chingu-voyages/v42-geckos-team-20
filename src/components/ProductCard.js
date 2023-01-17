e72b535c535b4b49d8570047f29c73c3a67e035c
bluemarket01 <highcla15@gmail.com>
2023-01-17 09:19:12 -0500

    Create productDetail Component

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,4 +1,8 @@
 import {Card, CardContent, CardMedia, Typography} from '@mui/material';
+import { Link } from "react-router-dom";
+import React from "react";
+
+
 
 const ProductCardStyles ={
 	border: "1px solid black",
@@ -13,14 +17,16 @@ const ProductCardStyles ={
 	margin: "1%",
 	padding:"1%"
 }
-export default function ProductCard ({name,seller,image}) {
+export default function ProductCard ({id,name,seller,image}) {
 	return(
-		<Card raised={true} sx={ProductCardStyles}>
-			<CardMedia
+		
+		<Card raised={true} sx={ProductCardStyles} key={id}>
+				<Link to={`/products/${id}`}>
+				<CardMedia
 				component="img"
-				image={image}
+				image={image} style={{width: '100%', height: '250px'}}
 				title={name}
-			/>
+			/></Link>
 			<CardContent>
 				<Typography>{name}</Typography>
 			</CardContent>
