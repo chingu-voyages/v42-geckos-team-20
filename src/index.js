2ac5f83d9375f2c25271e952fe2a678499c3a025
Julie Evans <juliemevans17@gmail.com>
2023-01-24 19:12:09 -0600

    add supabase client

--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ import App from './App';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
